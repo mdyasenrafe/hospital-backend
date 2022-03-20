57e31f2252b380f526744118f804292eb67b220e
mdyasenrafe <mdyasenrafe@gmail.com>
2022-03-20 15:11:36 +0600

    service delete

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 });
 // first route
 app.get("/", (req, res) => {
-  res.send("<h1>This is Medicare server</h1>");
+  res.send("This is Medicare server");
 });
 
 //   test
