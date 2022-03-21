3c7c12245505d60cbb976f017749a70d1eead531
mdyasenrafe <mdyasenrafe@gmail.com>
2022-03-21 15:50:06 +0600

    update review done

--- src/Routes/ReviewRoute.js
+++ src/Routes/ReviewRoute.js
@@ -3,11 +3,13 @@ const {
   getReview,
   addReview,
   deleteReview,
+  UpdateReview,
 } = require("../Controllers/ReviewController");
 const router = express.Router();
 
 router.post("/addReview", addReview);
 router.get("/getReview", getReview);
 router.post("/deleteReview", deleteReview);
+router.post("/updateReview", UpdateReview);
 
 module.exports = router;
